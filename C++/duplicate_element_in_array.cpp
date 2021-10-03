f699d74ffaba81f435a8fbd2a324fa5c2b7f31ae
Divyesh <divyeshkapoor6@gmail.com>
2021-10-03 17:37:09 +0530

    code changed using emplace and \n

--- C++/duplicate_element_in_array.cpp
+++ C++/duplicate_element_in_array.cpp
@@ -3,24 +3,26 @@ with exactly one element repeated in the array.
 Your task is to find the duplicate element.
 Time Complexity: O(nlogn)
 */
+
 #include<bits/stdc++.h>
 using namespace std;
 int main()
 {
     int test;
-    cout<<"Enter total testcases: "<<endl;
+    cout<<"Enter total testcases: \n";//  \n is faster than endl i have read it; 
     cin>>test;
     while(test--)
     {
         int n;
-        cout<<"Enter total: "<<endl;
+        cout<<"Enter size: \n";
         cin>>n;
+        cout<<"Enter elements which are in between 1 to "<<n-1<<" with one duplicate element :\n";
         vector<int> v;
         for(int i=0;i<n;i++)
         {
             int temp;
             cin>>temp;
-            v.push_back(temp);
+            v.emplace_back(temp);   //emplace_back() is faster operation than push_back();
         }
         unordered_map<int,int> mp;
         for(int i=0;i<n;i++)
@@ -31,9 +33,10 @@ int main()
         {
             if(itr.second>1)
             {
-                cout<<"The duplicate element is: "<<itr.first<<endl;
+                cout<<"The duplicate element is: "<<itr.first<<"\n";
             }
-        }
+        }   
     }
     return 0;
 }
+
